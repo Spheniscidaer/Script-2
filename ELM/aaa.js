ec57747637df8b8ecc5c9d374fddca896d6d8386
Luobook <92901153+LubooC@users.noreply.github.com>
2021-11-23 22:26:50 +0800

    Update aaa.js

--- ELM/aaa.js
+++ ELM/aaa.js
@@ -90,7 +90,7 @@ function fhxzck() {
     if ($request.url.indexOf("svip") > -1) {
         const elmck = $request.headers.Cookie
         $.log(elmck)
-        const elmck = $request.headers['Cookie']
+        elmck = $request.headers['Cookie']
         if (elmck) $.setdata(elmck, `elmck${status}`)
         $.log(elmck)
         $.msg($.name, "", `饿了么${status}数据获取成功`)
