ade194decc45e381ad69004dbbdcbfa04b3baa8a
Luobook <92901153+LubooC@users.noreply.github.com>
2021-11-23 20:44:21 +0800

    Update elm.js

--- ELM/elm.js
+++ ELM/elm.js
@@ -88,7 +88,7 @@ let num = rand(10, 99)
 //获取cookie 
 function fhxzck() {
     if ($request.url.indexOf("svip") > -1) {
-        const elmck = $request.headers['cookie']
+        const elmck = $request.headers['Cookie']
         if (elmck) $.setdata(elmck, `elmck${status}`)
         $.log(elmck)
         $.msg($.name, "", `饿了么${status}数据获取成功`)
